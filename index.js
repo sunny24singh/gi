5c3e6bd4cd456b98b7b3f670a7c24b22bd397182
sunny singh <sunny24singh@hotmail.com>
2024-09-14 19:04:46 +0530

    git commit -m "new data"

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ icon.addEventListener("click" , () => {
 
     
 })
+
+
+let hover = document.querySelector(".image-container_section3");
+
+hover.addEventListener('mouseover' , () => {
+    let d = document.querySelector(".section3_datas2");
+    d.style.backgroundImage = "https://thumbs.dreamstime.com/b/map-mountain-peaks-tops-coloured-geospatial-data-gis-usage-digital-elevation-model-gis-product-made-305203475.jpg"
+})
